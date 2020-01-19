ca7e4d922ccf335b942dd32e782b9fb739f36da9
Hemant Patidar <hemant.eblue@gmail.com>
2020-01-19 12:35:01 +0530

    Environments File Updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,5 +4,6 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 export const environment = {
-  production: true,
-};
+  production: false,
+  apiUrl:'https://thyme-dev.stamp-me.com/api/v2_noauth/',
+  thymeAPI_KEY: 'debug_key'};
